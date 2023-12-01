2076b111cc447e40106e8eecc89f7dde4926129b
Luuk-W <luuk.weiss@justeattakeaway.com>
2023-12-01 14:03:33 +0100

    add some puzzle setup to measure time of function execution and generic puzzle layout

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+from puzzles import puzzle1  # Import your puzzle modules
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def execute_puzzle(puzzle_module, input_file):
+    with open(input_file, 'r') as file:
+        input_data = file.read()
+
+    start_time = time.time()
+    answer_part1 = puzzle_module.solve_part1(input_data)
+    elapsed_time_part1 = int((time.time() - start_time) * 1000)
+
+    start_time = time.time()
+    answer_part2 = puzzle_module.solve_part2(input_data)
+    elapsed_time_part2 = int((time.time() - start_time) * 1000)
+
+    start_time = time.time()
+    answer_part3 = puzzle_module.solve_part3(input_data)
+    elapsed_time_part3 = int((time.time() - start_time) * 1000)
+
+    return answer_part1, elapsed_time_part1, answer_part2, elapsed_time_part2, answer_part3, elapsed_time_part3
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    input_file_puzzle1 = "puzzles/input_puzzle1.txt"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    answer1, time1, answer2, time2, answer3, time3 = execute_puzzle(puzzle1, input_file_puzzle1)
+    print(f"Day 1, Puzzle 1:\n\tAnswer Part 1: {answer1} [{time1}ms]\n\tAnswer Part 2: {answer2} [{time2}ms]\n\tAnswer Part 3: {answer3} [{time3}ms]")
---
+++ puzzles/puzzle1.py
@@ -0,0 +1,16 @@
+def solve_part1(input):
+    return "answer of puzzle 1"
+
+
+def solve_part2(input):
+    return "answer of puzzle 2"
+
+
+def solve_part3(input):
+    return "answer of puzzle 3"
+
+
+if __name__ == "__main__":
+    input_data_example = ...  # Provide an example input
+    print(solve_part1(input_data_example))
+    print(solve_part2(input_data_example))
