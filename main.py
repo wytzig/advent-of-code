b5273aead913edb33a2890694f6cddbc7906f305
Luuk-W <luuk.weiss@justeattakeaway.com>
2023-12-01 16:56:40 +0100

    solved day 1 part 2

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def execute_puzzle(puzzle_module, input_file, input_file2, input_file3, input_fi
         input_data = file.read()
     with open(input_file2, 'r') as file:
         input_data2 = file.read()
-    with open(input_file, 'r') as file:
+    with open(input_file3, 'r') as file:
         input_data3 = file.read()
-    with open(input_file, 'r') as file:
+    with open(input_file4, 'r') as file:
         input_data4 = file.read()
 
     start_time = time.time()
@@ -21,11 +21,11 @@ def execute_puzzle(puzzle_module, input_file, input_file2, input_file3, input_fi
     elapsed_time_part2 = int((time.time() - start_time) * 1000)
 
     start_time = time.time()
-    answer_part3 = puzzle_module.solve_part3(input_data)
+    answer_part3 = puzzle_module.solve_part3(input_data3)
     elapsed_time_part3 = int((time.time() - start_time) * 1000)
 
     start_time = time.time()
-    answer_part4 = puzzle_module.solve_part4(input_data)
+    answer_part4 = puzzle_module.solve_part4(input_data4)
     elapsed_time_part4 = int((time.time() - start_time) * 1000)
 
     return answer_part1, elapsed_time_part1, answer_part2, elapsed_time_part2, answer_part3, elapsed_time_part3, answer_part4, elapsed_time_part4
@@ -34,8 +34,7 @@ def execute_puzzle(puzzle_module, input_file, input_file2, input_file3, input_fi
 if __name__ == "__main__":
     input_file_puzzle1 = "puzzles/input_puzzle1-1.txt"
     input_file_puzzle2 = "puzzles/input_puzzle1-2.txt"
-    input_file_puzzle3 = "puzzles/input_puzzle1-1.txt"
-    input_file_puzzle4 = "puzzles/input_puzzle1-1.txt"
+    input_file_puzzle3 = "puzzles/input_puzzle1-3.txt"
 
-    answer1, time1, answer2, time2, answer3, time3, answer4, time4 = execute_puzzle(puzzle1, input_file_puzzle1, input_file_puzzle2, input_file_puzzle3, input_file_puzzle4)
+    answer1, time1, answer2, time2, answer3, time3, answer4, time4 = execute_puzzle(puzzle1, input_file_puzzle1, input_file_puzzle2, input_file_puzzle3, input_file_puzzle2)
     print(f"Day 1, Puzzle 1:\n\tAnswer Part 1: {answer1} [{time1}ms]\n\tAnswer Part 2: {answer2} [{time2}ms]\n\tAnswer Part 3: {answer3} [{time3}ms]\n\tAnswer Part 4: {answer4} [{time4}ms]")
--- puzzles/puzzle1.py
+++ puzzles/puzzle1.py
@@ -1,5 +1,22 @@
 import re
 
+DIGIT_MAPPING: dict = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+
+def get_first_and_last_digits(digits):
+    all_digits = "".join([DIGIT_MAPPING.get(digit, digit) for digit in digits])
+    return int(all_digits[0] + all_digits[-1])
+
 
 def solve_line(line):
     number = ""
@@ -17,24 +34,35 @@ def solve_line(line):
     return number
 
 
-def solve_part1(input):
+def solve_first(document_input):
     result = 0
-    for line in input.splitlines():
-        result = result + int(solve_line(line))
-    return result
-
-
-def solve_part2(input):
-    result = 0
-    for line in input.splitlines():
+    for line in document_input.splitlines():
         number_to_add = int(solve_line(line))
         result = result + number_to_add
     return result
 
 
-def solve_part3(input):
-    return "answer of puzzle 3"
+def solve_second(document_input):
+    result = 0
+    for line in document_input.splitlines():
+        print("line is: ", line)
+        digits = re.findall("(?=(one|two|three|four|five|six|seven|eight|nine|\d+))", line)
+        print("digits: ", digits)
+        result = result + get_first_and_last_digits(digits)
+    return result
 
 
-def solve_part4(input):
-    return "answer of puzzle 4"
+def solve_part1(document_input):
+    return solve_first(document_input)
+
+
+def solve_part2(document_input):
+    return solve_first(document_input)
+
+
+def solve_part3(document_input):
+    return solve_second(document_input)
+
+
+def solve_part4(document_input):
+    return solve_second(document_input)
