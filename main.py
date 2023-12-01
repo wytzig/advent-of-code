78b9b4da3c3d78c70b45e14eb14bbac585b9556c
Luuk-W <luuk.weiss@justeattakeaway.com>
2023-12-01 16:35:28 +0100

    implemented day 1 part 1

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import time
 from puzzles import puzzle1  # Import your puzzle modules
 
 
-def execute_puzzle(puzzle_module, input_file):
+def execute_puzzle(puzzle_module, input_file, input_file2, input_file3, input_file4):
     with open(input_file, 'r') as file:
         input_data = file.read()
+    with open(input_file2, 'r') as file:
+        input_data2 = file.read()
+    with open(input_file, 'r') as file:
+        input_data3 = file.read()
+    with open(input_file, 'r') as file:
+        input_data4 = file.read()
 
     start_time = time.time()
     answer_part1 = puzzle_module.solve_part1(input_data)
     elapsed_time_part1 = int((time.time() - start_time) * 1000)
 
     start_time = time.time()
-    answer_part2 = puzzle_module.solve_part2(input_data)
+    answer_part2 = puzzle_module.solve_part2(input_data2)
     elapsed_time_part2 = int((time.time() - start_time) * 1000)
 
     start_time = time.time()
@@ -26,7 +32,10 @@ def execute_puzzle(puzzle_module, input_file):
 
 
 if __name__ == "__main__":
-    input_file_puzzle1 = "puzzles/input_puzzle1.txt"
+    input_file_puzzle1 = "puzzles/input_puzzle1-1.txt"
+    input_file_puzzle2 = "puzzles/input_puzzle1-2.txt"
+    input_file_puzzle3 = "puzzles/input_puzzle1-1.txt"
+    input_file_puzzle4 = "puzzles/input_puzzle1-1.txt"
 
-    answer1, time1, answer2, time2, answer3, time3, answer4, time4 = execute_puzzle(puzzle1, input_file_puzzle1)
+    answer1, time1, answer2, time2, answer3, time3, answer4, time4 = execute_puzzle(puzzle1, input_file_puzzle1, input_file_puzzle2, input_file_puzzle3, input_file_puzzle4)
     print(f"Day 1, Puzzle 1:\n\tAnswer Part 1: {answer1} [{time1}ms]\n\tAnswer Part 2: {answer2} [{time2}ms]\n\tAnswer Part 3: {answer3} [{time3}ms]\n\tAnswer Part 4: {answer4} [{time4}ms]")
--- puzzles/puzzle1.py
+++ puzzles/puzzle1.py
@@ -1,9 +1,35 @@
+import re
+
+
+def solve_line(line):
+    number = ""
+    for character in line:
+        if number == "":
+            if character.isnumeric():
+                number = number + character
+    matches = re.finditer(r'\d', line)
+
+    last_digit = None
+    for match in matches:
+        last_digit = match.group()
+
+    number = number + last_digit
+    return number
+
+
 def solve_part1(input):
-    return "answer of puzzle 1"
+    result = 0
+    for line in input.splitlines():
+        result = result + int(solve_line(line))
+    return result
 
 
 def solve_part2(input):
-    return "answer of puzzle 2"
+    result = 0
+    for line in input.splitlines():
+        number_to_add = int(solve_line(line))
+        result = result + number_to_add
+    return result
 
 
 def solve_part3(input):
